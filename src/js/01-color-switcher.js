2a149ef83aaf83bb9ac71ef254265ad4eaf4555d
Andrey Shyshko <andrey.shyshko84@gmail.com>
2022-01-03 13:42:44 +0200

    wrote hw scripts and install libs

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
